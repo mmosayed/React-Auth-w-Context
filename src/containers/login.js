881c0beb88b26fa18dc3a8e81507bd5819fc357d
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-03-23 15:28:06 -0400

    login with context

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import firebase from '../firebase';
+import AuthContext from '../contexts/auth';
+import { Redirect } from 'react-router-dom';
 
 export default class Login extends React.Component {
 
@@ -30,23 +32,34 @@ export default class Login extends React.Component {
   render() {
     const { email, password, error } = this.state;
     const displayError = error === '' ? '' : <div className="alert alert-danger" role="alert">{error}</div>
+    const displayForm = <>
+      <h1>Login</h1>
+      {displayError}
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Login</button>
+      </form>
+    </>;
 
     return (
-      <>
-        <h1>Login</h1>
-        {displayError}
-        <form>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={email} onChange={this.handleChange} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" placeholder="Password" value={password} name="password" onChange={this.handleChange} />
-          </div>
-          <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Login</button>
-        </form>
-      </>
+      <AuthContext.Consumer>
+        {
+          (user) => {
+            if (user) {
+              return <Redirect to='/' />
+            } else {
+              return displayForm;
+            }
+          }
+        }
+      </AuthContext.Consumer>
     )
   }
 }
