f4a5289f03b445dd93cde5aa7ae4098da89cddb8
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-03-23 15:44:18 -0400

    home with context

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import AuthContext from '../contexts/auth';
 
 export default class Home extends React.Component {
   
-
   render() {
     return (
-      <>
-        <h2>Welcome back, </h2>
-        <h4>Your ID is: </h4>
-      </>
+      <AuthContext.Consumer>
+        {
+          (user) => {
+            if (user) {
+              return (
+                <>
+                  <h2>Welcome back, {user.email}</h2>
+                  <h4>Your ID is: {user.uid}</h4>
+                </>
+              )
+            } else {
+              return <h2>You are not logged in.</h2>
+            }
+          }
+        }
+      </AuthContext.Consumer>
     )
   }
 }
