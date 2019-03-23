a17b3a19ee39842eb7bc4e26342a5ea96a447f81
Mohammed Mosayed <mohammedmosayed@gmail.com>
2019-03-23 15:51:41 -0400

    header with context

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../contexts/auth';
 
 export default (props) => {
+  const loggedOut = <><li className="nav-item">
+      <Link className="nav-link" to="/signup">Sign Up</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/login">Login</Link>
+    </li></>
+
+  const loggedIn = <><li className="nav-item">
+      <Link className="nav-link" to="/logout">Logout</Link>
+    </li></>
+
   return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">Authentication</Link>
@@ -9,15 +21,14 @@ export default (props) => {
         <li className="nav-item">
           <Link className="nav-link" to="/">Home</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/signup">Sign Up</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Login</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/logout">Logout</Link>
-        </li>
+        <AuthContext.Consumer>
+          {
+            user => {
+              if (user) return loggedIn
+              else return loggedOut
+            }
+          }
+        </AuthContext.Consumer>
       </ul>
     </nav>
   )
